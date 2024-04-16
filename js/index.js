3f0b508bbfd725930e5d4a54cab33d268da07caa
Lucas Barrientos <lbm410@inlumine.ual.es>
2024-04-16 16:02:53 +0200

    Actualizacion scrolldown

--- js/index.js
+++ js/index.js
@@ -29,16 +29,19 @@ document.addEventListener("DOMContentLoaded", function () {
     const langs = document.querySelector(".langs");
     const links = document.querySelectorAll(".idioma-desplegable a");
     const linkedin = document.getElementById("linkedin");
+    const github = document.getElementById("github");
     const sobre_mi_description = document.getElementById("sobre_mi_description");
 
     let data = {
         english: {
             linkedin: "Visit my LinkedIn",
+            github: "Visit my GitHub",
             sobre_mi_description: "Hello! I'm Lucas, a third-year student of <mark>Computer Engineering</mark> at the <strong>University of Almería</strong>. Over these three years, I have built a solid foundation in <strong>programming</strong>, developing skills in languages such as <mark>Java</mark>, <mark>C#</mark>, <mark>JavaScript</mark>, and in the use of <mark>HTML</mark> and <mark>CSS</mark>, adept at handling tools like <mark>Visual Studio</mark> and <mark>Eclipse</mark>.",
             // Agrega otros textos en inglés aquí
         },
         spanish: {
             linkedin: "Visita mi LinkedIn",
+            github: "Visit my GitHub",
             sobre_mi_description: "¡Hola! Soy Lucas, un estudiante de tercer año de <mark>Ingeniería Informática</mark> en la <strong>Universidad de Almería</strong>. En estos tres años, he construido una sólida base en <strong>programación</strong>, desarrollando habilidades en lenguajes como <mark>Java</mark>, <mark>C#</mark>, <mark>JavaScript</mark> y en el uso de <mark>HTML</mark> y <mark>CSS</mark>, manejándome con soltura en herramientas como <mark>Visual Studio</mark> y <mark>Eclipse</mark>.",
 
             // Agrega otros textos en español aquí
