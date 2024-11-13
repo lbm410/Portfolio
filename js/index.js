b45ec2901a869ae6bda5febf8b18ef05a4eb6efd
Lucas Barrientos Muñoz <lbm410@inlumine.ual.es>
2024-11-13 17:08:51 +0100

    Portfolio Update

--- js/index.js
+++ js/index.js
@@ -22,9 +22,85 @@ darkMode.addEventListener("click", () => {
   darkMode.querySelector("span:nth-child(2)").classList.toggle("active");
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-    const langs = document.querySelector(".langs");
-    const links = document.querySelectorAll(".idioma-desplegable a");
+let container = document.getElementById('select-container');
+let items = container.getElementsByTagName('ul')[0].getElementsByTagName('li');
+let selectedItem = items[0];
+
+// Datos de traducciones
+let data = {
+    EN: {
+        linkedin: "Visit my LinkedIn",
+        github: "Visit my GitHub",
+        inicio: "Home",
+        proyectos: "Projects",
+        sobre_mi_title : "About me",
+        proyectos_title: "Projects",
+        tecnologias_utilizadas: "Technologies used in the project:",
+        sobre_mi: "About me",
+        sobre_mi_description: "Hi! I'm Lucas, a fourth-year student of <mark>Computer Engineering</mark> at the <strong>University of Almería</strong>. In these three years, I have built a solid foundation in <strong>programming</strong>, developing skills in languages such as <mark>Java</mark>, <mark>C#</mark>, <mark>JavaScript</mark>, and in the use of <mark>HTML</mark> and <mark>CSS</mark>, proficiently using tools like <mark>Visual Studio</mark> and <mark>Eclipse</mark>.",
+        sobre_mi_p2: "And although I could tell you that I handle basic Assembly (and it's true), what I really want you to know about me is that I am a tireless learner and a technology enthusiast, always willing to grow and overcome new challenges. I am motivated by the idea of combining my passion for technology with a tangible impact on people and companies.",
+        sobre_mi_p3: "If you are looking for someone who not only codes but also understands the meaning of their work, is open to learning, and always seeks ways to improve, I would love to connect with you and explore possible opportunities.",
+        quetal: "Hey, What's up?",
+        tecnologias_manejo: "<mark>Technologies</mark> I handle:",
+    },
+    ES: {
+        linkedin: "Visita mi LinkedIn",
+        github: "Visita mi GitHub",
+        inicio: "Inicio",
+        proyectos: "Proyectos",
+        sobre_mi_title : "Sobre mi",
+        proyectos_title: "Proyectos",
+        tecnologias_utilizadas: "Tecnologías utilizadas en el proyecto:",
+        sobre_mi: "Sobre mi",
+        sobre_mi_description: "¡Hola! Soy Lucas, un estudiante de cuarto año de <mark>Ingeniería Informática</mark> en la <strong>Universidad de Almería</strong>. En estos tres años, he construido una sólida base en <strong>programación</strong>, desarrollando habilidades en lenguajes como <mark>Java</mark>, <mark>C#</mark>, <mark>JavaScript</mark> y en el uso de <mark>HTML</mark> y <mark>CSS</mark>, manejándome con soltura en herramientas como <mark>Visual Studio</mark> y <mark>Eclipse</mark>.",
+        sobre_mi_p2: "Y aunque te podría decir que manejo Ensamblador básico (y es cierto), lo que realmente quiero que sepas de mí es que soy un aprendiz incansable y un entusiasta de la tecnología, siempre dispuesto a crecer y a superar nuevos retos. Me motiva la idea de combinar mi pasión por la tecnología con un impacto tangible en las personas y las empresas.",
+        sobre_mi_p3: "Si estás buscando a alguien que no solo codifique, sino que comprenda el significado de su trabajo, esté abierto al aprendizaje y siempre busque formas de mejorar, me encantaría conectarme contigo y explorar posibles oportunidades.",
+        quetal: "Hey, ¿Qué tal?",
+        tecnologias_manejo: "<mark>Tecnologías</mark> que manejo:",
+    }
+};
+
+hideSelected();
+
+function onSelect(item) {
+    showUnselected();
+    selectedItem.innerHTML = item.innerHTML;
+    selectedItem.setAttribute('lang-selection', item.getAttribute('lang-selection'));
+    selectedItem.setAttribute('tooltip', item.getAttribute('tooltip'));
+    hideSelected();
+    unwrapSelector();
+    updateLanguage(item.getAttribute('lang-selection'));
+}
+
+function unwrapSelector() {
+    container.style.pointerEvents = "none";
+    setTimeout(() => container.style.pointerEvents = "auto", 200);
+}
+
+function showUnselected() {
+    let selectedLangCode = selectedItem.getAttribute('lang-selection');
+    for (let i = 1; i < items.length; i++) {
+        if (items[i].getAttribute('lang-selection') == selectedLangCode) {
+            items[i].style.opacity = '1';
+            items[i].style.display = '';
+            break;
+        }
+    }
+}
+
+function hideSelected() {
+    let selectedLangCode = selectedItem.getAttribute('lang-selection');
+    for (let i = 1; i < items.length; i++) {
+        if (items[i].getAttribute('lang-selection') == selectedLangCode) {
+            items[i].style.opacity = '0';
+            setTimeout(() => items[i].style.display = 'none', 200);
+            break;
+        }
+    }
+}
+
+// Función para actualizar el contenido del sitio web según el idioma seleccionado
+function updateLanguage(langCode) {
     const linkedin = document.getElementById("linkedin");
     const github = document.getElementById("github");
     const proyectos = document.getElementById("proyectos");
@@ -39,83 +115,20 @@ document.addEventListener("DOMContentLoaded", function () {
     const tecnologias_manejo = document.getElementById("tecnologias_manejo");
     const quetal = document.getElementById("quetal");
 
-    let data = {
-        english: {
-            linkedin: "Visit my LinkedIn",
-            github: "Visit my GitHub",
-            inicio: "Home",
-            proyectos: "Projects",
-            sobre_mi_title : "About me",
-            proyectos_title: "Projects",
-            tecnologias_utilizadas: "Technologies used in the project:",
-            sobre_mi: "About me",
-            sobre_mi_description: "Hi! I'm Lucas, a fourth-year student of <mark>Computer Engineering</mark> at the <strong>University of Almería</strong>. In these three years, I have built a solid foundation in <strong>programming</strong>, developing skills in languages such as <mark>Java</mark>, <mark>C#</mark>, <mark>JavaScript</mark>, and in the use of <mark>HTML</mark> and <mark>CSS</mark>, proficiently using tools like <mark>Visual Studio</mark> and <mark>Eclipse</mark>.",
-            sobre_mi_p2: "And although I could tell you that I handle basic Assembly (and it's true), what I really want you to know about me is that I am a tireless learner and a technology enthusiast, always willing to grow and overcome new challenges. I am motivated by the idea of combining my passion for technology with a tangible impact on people and companies.",
-            sobre_mi_p3: "If you are looking for someone who not only codes but also understands the meaning of their work, is open to learning, and always seeks ways to improve, I would love to connect with you and explore possible opportunities.",
-            quetal: "Hey, What's up?",
-            tecnologias_manejo: "<mark>Technologies</mark> I handle:",
-        },
-        spanish: {
-            linkedin: "Visita mi LinkedIn",
-            github: "Visita mi GitHub",
-            inicio: "Inicio",
-            proyectos: "Proyectos",
-            sobre_mi_title : "Sobre mi",
-            proyectos_title: "Proyectos",
-            tecnologias_utilizadas: "Tecnologías utilizadas en el proyecto:",
-            sobre_mi: "Sobre mi",
-            sobre_mi_description: "¡Hola! Soy Lucas, un estudiante de cuarto año de <mark>Ingeniería Informática</mark> en la <strong>Universidad de Almería</strong>. En estos tres años, he construido una sólida base en <strong>programación</strong>, desarrollando habilidades en lenguajes como <mark>Java</mark>, <mark>C#</mark>, <mark>JavaScript</mark> y en el uso de <mark>HTML</mark> y <mark>CSS</mark>, manejándome con soltura en herramientas como <mark>Visual Studio</mark> y <mark>Eclipse</mark>.",
-            sobre_mi_p2: "Y aunque te podría decir que manejo Ensamblador básico (y es cierto), lo que realmente quiero que sepas de mí es que soy un aprendiz incansable y un entusiasta de la tecnología, siempre dispuesto a crecer y a superar nuevos retos. Me motiva la idea de combinar mi pasión por la tecnología con un impacto tangible en las personas y las empresas.",
-            sobre_mi_p3: "Si estás buscando a alguien que no solo codifique, sino que comprenda el significado de su trabajo, esté abierto al aprendizaje y siempre busque formas de mejorar, me encantaría conectarme contigo y explorar posibles oportunidades.",
-            quetal: "Hey, ¿Qué tal?",
-            tecnologias_manejo: "<mark>Tecnologías</mark> que manejo:",
-        },
-    };
+    linkedin.textContent = data[langCode].linkedin;
+    github.textContent = data[langCode].github;
+    inicio.textContent = data[langCode].inicio;
+    proyectos.textContent = data[langCode].proyectos;
+    sobre_mi_title.textContent = data[langCode].sobre_mi_title;
+    sobre_mi.textContent = data[langCode].sobre_mi;
+    sobre_mi_description.innerHTML = data[langCode].sobre_mi_description;
+    sobre_mi_p2.innerHTML = data[langCode].sobre_mi_p2;
+    sobre_mi_p3.innerHTML = data[langCode].sobre_mi_p3;
+    proyectos_title.textContent = data[langCode].proyectos_title;
+    quetal.textContent = data[langCode].quetal;
+    tecnologias_manejo.innerHTML = data[langCode].tecnologias_manejo;
 
-    links.forEach((el) => {
-        el.addEventListener("click", (e) => {
-            e.preventDefault();            
-            const activeElement = langs.querySelector(".active");
-            if (activeElement) {
-                activeElement.classList.remove("active");
-            }
-            
-            el.classList.add("active");
-    
-            const attr = el.dataset.lang;
-    
-            linkedin.textContent = data[attr].linkedin;
-
-            github.textContent = data[attr].github;
-
-            proyectos.textContent = data[attr].proyectos;
-
-            inicio.textContent = data[attr].inicio;
-
-            sobre_mi_title.textContent = data[attr].sobre_mi_title;
-
-            sobre_mi.textContent = data[attr].sobre_mi;
-
-            sobre_mi_description.innerHTML = data[attr].sobre_mi_description;
-
-            sobre_mi_p2.innerHTML = data[attr].sobre_mi_p2;
-
-            sobre_mi_p3.innerHTML = data[attr].sobre_mi_p3;
-
-            proyectos_title.textContent = data[attr].proyectos_title;
-
-            quetal.textContent = data[attr].quetal;
-
-            tecnologias_manejo.innerHTML = data[attr].tecnologias_manejo;
-
-            tecnologias_utilizadas.forEach(element => {
-                element.textContent = data[attr].tecnologias_utilizadas;
-            });
-        });
+    tecnologias_utilizadas.forEach(element => {
+        element.textContent = data[langCode].tecnologias_utilizadas;
     });
-    
-});
-
-
-
-
+}
